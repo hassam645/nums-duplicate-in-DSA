e74f888395bbe2c1e2d9a85b4e4cb789dc4cddca
hassam bin shahid <139114049+hassam645@users.noreply.github.com>
2024-04-18 23:21:37 -0700

    Add files via upload

---
+++ nums duplicate.12py.py
@@ -0,0 +1,9 @@
+class Solution:
+def containsDuplicate(self, nums: List[int]) -> bool:
+s=set()
+for ele in nums:
+  if ele in s:
+    return True
+  else:
+    s.add(ele)
+    return false
---
+++ nums duplicate.13py.py
@@ -0,0 +1,7 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+  n=len(nums)
+  for i in range(n-1):
+  for j in range(i+1,n):
+  if nums[i]==nums[j]:
+    return True
+    return false
